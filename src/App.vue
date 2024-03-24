b247d22aa7579093fff43222aaf40831903fb54c
LeonidevFR <leonardploton.dev@gmail.com>
2024-03-24 14:45:32 +0100

    Responsive & media queries

--- src/App.vue
+++ src/App.vue
@@ -18,25 +18,4 @@ export default {
 
 <style lang="scss">
 @import "assets/styles/app.scss";
-
-#app {
-  @extend .text--medium;
-  font-family: "tt_chocolates_medium", Arial, Helvetica, sans-serif;
-  height: 100%;
-}
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-body {
-  margin: 0;
-}
-html,
-body {
-  width: 100%;
-  height: 100%;
-  background-color: #f5faff;
-}
 </style>
